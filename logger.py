69fba3ddbf1f0ff8a9105b2a35563747b5af9431
IsaacNew85 <104782758+IsaacNew85@users.noreply.github.com>
2022-05-03 11:55:22 +0800

    Update logger.py

--- logger.py
+++ logger.py
@@ -4,7 +4,8 @@ import torch.nn.functional as F
 import imageio
 
 import os
-from skimage.draw import circle
+#from skimage.draw import circle
+from skimage.draw import disk as circle
 
 import matplotlib.pyplot as plt
 import collections
